1cbe7b88b77953f924a005b39258d73b270fbb02
Aary Vijay Jadhav <aary.jadhav18@gmail.com>
2024-02-20 19:00:34 +0530

    Update data.dart

--- lib/About Us/data.dart
+++ lib/About Us/data.dart
@@ -37,22 +37,4 @@ List<PlanetInfo> planets = [
         'https://i.postimg.cc/dQXrnLvk/Untitled-design-14.png',
         'https://i.postimg.cc/j52n4rF4/Untitled-design-15.png',
       ]),
-  PlanetInfo(3,
-      name: 'Snowy Fernandez',
-      iconImage: 'assets/snowypp.png',
-      description:
-      "\u2022 Name:- Snowy Ashok Fernandez\n\u2022 Group No:-3\n\u2022 Class:- TYCO-1\n\u2022 Roll No:- 1810\n\u2022 College:- Bhausaheb Vartak Polytechnic, Vasai(W), Palghar- 401202",
-      images: [
-        'https://i.postimg.cc/QMy1Pt5y/Untitled-design-17.png',
-        'https://i.postimg.cc/WzRrNH5m/Untitled-design-18.png',
-      ]),
-  PlanetInfo(4,
-      name: 'Ashal          \nDabre',
-      iconImage: 'assets/ashalpp.png',
-      description:
-      "\u2022 Name:- Ashal Ashok Fernandez\n\u2022 Group No:-3\n\u2022 Class:- TYCO-1\n\u2022 Roll No:- 18\n\u2022 College:- Bhausaheb Vartak Polytechnic, Vasai(W), Palghar- 401202",
-      images: [
-        'https://i.postimg.cc/4dpy5N1t/Untitled-design-21.png',
-        'https://i.postimg.cc/zXxBpQTd/Untitled-design-16.png',
-      ]),
 ];
